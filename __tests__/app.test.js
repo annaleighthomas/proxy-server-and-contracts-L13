be1c5330ce1de697114ee28192badfa9eb016d7d
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-05-12 19:13:37 -0400

    passing location test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,19 +1,54 @@
-
+import locationData from '../data/location';
+import { formatLocation, formatWeather } from '../lib/munge-utils';
 
 
 describe('API Data Munging', () => {
 
-  const expectedMovies = [];
+  const expectedLocation = [
+    {
+      formatted_query: 'Portland, Multnomah County, Oregon, USA',
+      latitude: '45.5202471',
+      longitude: '-122.6741949'
+    },
+    {
+      formatted_query: 'Portland, Cumberland County, Maine, USA',
+      latitude: '43.6610277',
+      longitude: '-70.2548596'
+    },
+    {
+      formatted_query: 'Portland, San Patricio County, Texas, USA',
+      latitude: '27.8768086',
+      longitude: '-97.3233874'
+    }
+  ];
     
-  test('GET /api/movies', async () => {
-    // act - make the request
-    const response = await request.get('/api/cats');
+  test('munges location data', async () => {
+    
+    const output = formatLocation(locationData);
 
-    // was response OK (200)?
-    expect(response.status).toBe(200);
-
-    // did it return the data we expected?
-    expect(response.body).toEqual(expectedMovies);
+    expect(output).toEqual(expectedLocation);
 
   });
+
+  const expectedWeather = [
+
+    {
+      forecast:
+    }
+
+  ];
+
+
+  // [
+  //   {
+  //     "forecast": "Partly cloudy until afternoon.",
+  //     "time": "Mon Jan 01 2001"
+  //   },
+  //   {
+  //     "forecast": "Mostly cloudy in the morning.",
+  //     "time": "Tue Jan 02 2001"
+  //   },
+  //   ...
+  // ]
+
 });
