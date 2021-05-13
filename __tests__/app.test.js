9abf968511e388be43ffd941f3bc79f51b4769bb
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-05-12 23:51:13 -0400

    app.js

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -6,23 +6,12 @@ import { formatLocation, formatWeather, formatYelp } from '../lib/munge-utils';
 
 describe('API Data Munging', () => {
 
-  const expectedLocation = [
-    {
-      formatted_query: 'Portland, Multnomah County, Oregon, USA',
-      latitude: '45.5202471',
-      longitude: '-122.6741949'
-    },
-    {
-      formatted_query: 'Portland, Cumberland County, Maine, USA',
-      latitude: '43.6610277',
-      longitude: '-70.2548596'
-    },
-    {
-      formatted_query: 'Portland, San Patricio County, Texas, USA',
-      latitude: '27.8768086',
-      longitude: '-97.3233874'
-    }
-  ];
+  const expectedLocation = {
+    formatted_query: 'Portland, Multnomah County, Oregon, USA',
+    latitude: '45.5202471',
+    longitude: '-122.6741949'
+  };
+
     
   test('munges location data', async () => {
     
